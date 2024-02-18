359c2aff0c607485ac2da7a1268f1088f6c0cdf0
webdevkirill <krill.fyodorov@gmail.com>
2024-02-18 19:45:26 +0300

    add BundleAnalyzerPlugin and refactor build plugins func

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,22 +1,34 @@
 import HTMLWebpackPlugin from 'html-webpack-plugin';
-import MiniSccExtractPlugin from 'mini-css-extract-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 import { IBuildOptions } from './buildTypes';
 
-export const buildPlugins = ({ paths, isDev }: IBuildOptions): webpack.WebpackPluginInstance[] => {
-  return [
-    new HTMLWebpackPlugin({
-      template: paths.html,
-    }),
+export const buildPlugins = (options: IBuildOptions): webpack.WebpackPluginInstance[] => {
+  const plugins: webpack.WebpackPluginInstance[] = [
     new webpack.ProgressPlugin(),
-    new MiniSccExtractPlugin({
+    new HTMLWebpackPlugin({
+      template: options.paths.html,
+    }),
+    new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev),
+      __IS_DEV__: JSON.stringify(options.isDev),
     }),
-    new webpack.HotModuleReplacementPlugin(),
   ];
+
+  if (options.isDev) {
+    plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      // @ts-ignore-next-line
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      }),
+    );
+  }
+
+  return plugins;
 };
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,7 +15,7 @@ export const App = () => {
 
   return (
     <div className={classNames('app', undefined, [theme])}>
-      <Suspense fallback="f">
+      <Suspense fallback="">
         <Navbar />
         <div className="content-page">
           <Sidebar />
