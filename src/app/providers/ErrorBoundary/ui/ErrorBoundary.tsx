47eab10487ebbd28581d25022e61f23d5b479e17
MacMini <macmini@Mac-mini-MacMini.local>
2024-02-18 19:21:31 +0300

    add ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,45 @@
+import React, { ErrorInfo, Suspense } from 'react';
+
+import { PageError } from 'widgets/PageError';
+
+interface IErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface IErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<IErrorBoundaryProps, IErrorBoundaryState> {
+  constructor(props: IErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return (
+        <Suspense fallback="">
+          <PageError />
+        </Suspense>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,25 @@
+import { useTranslation } from 'react-i18next';
+
+import { classNames } from 'shared/lib/classNames';
+import { Button } from 'shared/ui/Button';
+
+import cls from './PageError.module.scss';
+
+interface IPageErrorProps {
+  className?: string;
+}
+
+export const PageError: React.FC<IPageErrorProps> = props => {
+  const { t } = useTranslation();
+
+  const handleReloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className={classNames(cls.PageError, {}, [props.className])}>
+      <p>{t('page-error')}</p>
+      <Button onClick={handleReloadPage}>{t('buttons.reload-page')}</Button>
+    </div>
+  );
+};
